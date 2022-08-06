fc4375bb025dfe3a49c87b9a8cef5ee461032bef
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-08-06 18:42:01 +0200

    Add config for absolute paths imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '~', replacement: path.resolve(__dirname, '/src') }],
+  },
   plugins: [react()]
 });
