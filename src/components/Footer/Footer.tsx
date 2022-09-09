2d63cd67c04c99b9a5b28d40411f7d3830a53c7c
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-09-09 18:53:30 +0200

    Add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import { Footer as StyledFooter } from './Footer.style';
+
+export const Footer = () => {
+  return (
+    <StyledFooter>
+      <Typography variant='caption'>
+        Copyright &copy; 2022. Made by Klaudia, Julia, Monika, Justyna
+      </Typography>
+    </StyledFooter>
+  );
+};
