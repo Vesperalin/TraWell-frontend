460fa4bbdd32c0b8e8f8550e4e4fe3a67d696724
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-09-29 21:57:45 +0200

    Add service

--- src/api/services/Osmnames.ts
+++ src/api/services/Osmnames.ts
@@ -4,9 +4,15 @@ import { OsmnamesPlace } from '~/models/OsmnamesPlace';
 
 export default {
   useAutocompletePlaces: (phrase: string) => {
-    return useQuery<OsmnamesPlace[], Error>(['phrase', phrase], async () => {
-      const response = await osmnamesCityAutocompleteClient.get(`${phrase}.js`);
-      return response.data.results;
-    });
+    return useQuery<OsmnamesPlace[], Error>(
+      ['phrase', phrase],
+      async () => {
+        const response = await osmnamesCityAutocompleteClient.get(`${phrase}.js`);
+        return response.data.results;
+      },
+      {
+        enabled: false,
+      },
+    );
   },
 };
