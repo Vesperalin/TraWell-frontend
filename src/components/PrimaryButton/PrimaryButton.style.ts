931d5006d597b4ccfd38e2025b5b7087d3d7471b
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-04 14:16:35 +0200

    Update primary button

--- src/components/PrimaryButton/PrimaryButton.style.ts
+++ src/components/PrimaryButton/PrimaryButton.style.ts
@@ -2,13 +2,10 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/system';
 
 export const StyledPrimaryButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.common.navy,
+  backgroundColor: theme.palette.common.blue,
   color: theme.palette.common.dimmedWhite,
 
   '&:hover': {
     backgroundColor: '#0252CE',
   },
-  // [theme.breakpoints.down('md')]: {
-  //   padding: '20px',
-  // },
 }));
