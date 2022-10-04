88ff12154dc08e1f8487191529b27611486bc30b
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-04 14:58:46 +0200

    update amount of people input

--- src/components/AmountOfPeopleInput/AmountOfPeopleInput.tsx
+++ src/components/AmountOfPeopleInput/AmountOfPeopleInput.tsx
@@ -3,12 +3,12 @@ import InputAdornment from '@mui/material/InputAdornment';
 import { Theme, useTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect } from 'react';
 import { useStyles } from './AmountOfPeopleInput.style';
 
 interface Props {
-  amountOfPeople: number;
-  setAmountOfPeople: (amount: number) => void;
+  amountOfPeople: string | null;
+  setAmountOfPeople: (amount: string | null) => void;
 }
 
 export const AmountOfPeopleInput = ({ amountOfPeople, setAmountOfPeople }: Props) => {
@@ -16,8 +16,25 @@ export const AmountOfPeopleInput = ({ amountOfPeople, setAmountOfPeople }: Props
   const { input } = useStyles(theme);
   const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
 
+  useEffect(() => {
+    // eslint-disable-next-line prettier/prettier
+    const element = document.querySelector('input[type=\'number\']');
+
+    if (element !== null) {
+      element.addEventListener('keydown', (evt) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        if ((evt as any).which === 8) return;
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        if ((evt as any).which < 48 || (evt as any).which > 57) {
+          evt.preventDefault();
+        }
+      });
+    }
+  }, []);
+
   const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setAmountOfPeople(Number(event.target.value));
+    setAmountOfPeople(event.target.value.trim());
   };
 
   return (
@@ -28,6 +45,7 @@ export const AmountOfPeopleInput = ({ amountOfPeople, setAmountOfPeople }: Props
       className={input}
       size={isSmallScreen ? 'small' : 'medium'}
       InputProps={{
+        inputMode: 'numeric',
         startAdornment: (
           <InputAdornment position='start'>
             <AccountCircle />
