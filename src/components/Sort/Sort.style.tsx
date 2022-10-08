b8a9ca904dce3b45b1a6fbffae213d60074012ca
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-08 12:32:05 +0200

    ZT-35-FE-Create-sort-by-component (#19)

---
+++ src/components/Sort/Sort.style.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/system';
+
+export const StyledSorter = styled(Box)({
+  minWidth: '150px',
+  marginTop: '50px',
+});
+
+export const StyledTypography = styled(Typography)({
+  margin: '5px',
+});
