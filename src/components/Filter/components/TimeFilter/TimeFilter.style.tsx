083b9d8475d380dd764a0d72c15233833dd36471
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-09 17:19:44 +0200

    add time filter

---
+++ src/components/Filter/components/TimeFilter/TimeFilter.style.tsx
@@ -0,0 +1,13 @@
+import { Theme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles<Theme>((theme: Theme) => ({
+  timePicker: {
+    borderRadius: '10px',
+    backgroundColor: theme.palette.common.dimmedWhite,
+    color: theme.palette.common.darkNavy,
+    width: '100%',
+  },
+}));
+
+export { useStyles };
