82a3beeb06818e550c864babbf7d2ac50a59b6c3
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-09 23:02:14 +0200

    Add general styles for app

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -1,15 +1,10 @@
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 
-export const Wrapper = styled(Box)(({ theme }) => ({
+export const Wrapper = styled(Box)({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  margin: '50px 0 20px 0',
   width: '100%',
-
-  [theme.breakpoints.down('md')]: {
-    margin: '30px 0',
-  },
-}));
+});
