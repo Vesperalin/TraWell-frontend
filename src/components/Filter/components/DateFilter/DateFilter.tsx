3b34a1ce3741f81a0bbf941c07b789308a00d19e
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-09 16:56:09 +0200

    Add datefilter

---
+++ src/components/Filter/components/DateFilter/DateFilter.tsx
@@ -0,0 +1,63 @@
+import { Theme, useTheme } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { Dayjs } from 'dayjs';
+import { DateFilterType } from '../../models/FilterType';
+import { useStyles } from './DateFilter.style';
+
+interface Props {
+  filter: DateFilterType;
+}
+
+export const DateFilter = ({ filter }: Props) => {
+  const { value, setValue } = filter;
+  const theme = useTheme();
+  const { dateFilter } = useStyles(theme);
+  const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+
+  const handleDateChange = (newDate: Dayjs | null) => {
+    setValue(newDate);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      {isSmallScreen ? (
+        <MobileDatePicker
+          label='Date'
+          className={dateFilter}
+          inputFormat='DD/MM/YYYY'
+          value={value}
+          onChange={handleDateChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              size='small'
+              variant='outlined'
+              placeholder='Date'
+            />
+          )}
+        />
+      ) : (
+        <DesktopDatePicker
+          label='Date'
+          inputFormat='DD/MM/YYYY'
+          className={dateFilter}
+          value={value}
+          onChange={handleDateChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              size='small'
+              variant='outlined'
+              placeholder='Date'
+            />
+          )}
+        />
+      )}
+    </LocalizationProvider>
+  );
+};
