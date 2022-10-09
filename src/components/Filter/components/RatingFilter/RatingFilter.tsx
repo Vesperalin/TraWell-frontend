a117974a9502efe72f9d59bda9c53bcb19cf55bd
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-09 20:26:46 +0200

    Add rating filter

---
+++ src/components/Filter/components/RatingFilter/RatingFilter.tsx
@@ -0,0 +1,25 @@
+import FormControl from '@mui/material/FormControl';
+import Rating from '@mui/material/Rating';
+import { RatingFilterType } from '../../models/FilterType';
+import { StyledFormLabel } from './RatingFilter.style';
+
+interface Props {
+  filter: RatingFilterType;
+}
+
+export const RatingFilter = ({ filter }: Props) => {
+  const { value, setValue } = filter;
+
+  return (
+    <FormControl>
+      <StyledFormLabel>Driver review:</StyledFormLabel>
+      <Rating
+        name='simple-controlled'
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </FormControl>
+  );
+};
