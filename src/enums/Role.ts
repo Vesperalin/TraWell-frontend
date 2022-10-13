1cc7e76192fdfc964a6c1195ae62d1fc8597a908
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-13 19:55:12 +0200

    Add roles in frontend

---
+++ src/enums/Role.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  Private = 'private_user',
+  Company = 'company_user',
+  User = 'user',
+}
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,11 @@
 import { useKeycloak } from '@react-keycloak/web';
+import { Role } from '~/enums/Role';
 
 export const useAuth = () => {
   const { keycloak } = useKeycloak();
-  const { authenticated, login, logout, hasRealmRole, hasResourceRole } = keycloak;
+  const { authenticated, login, logout, hasRealmRole, hasResourceRole, token } = keycloak;
 
-  const hasRole = (role: string) => hasRealmRole(role) || hasResourceRole(role);
+  const hasRole = (role: Role) => hasRealmRole(role) || hasResourceRole(role);
 
-  return { authenticated, login, logout, hasRole };
+  return { authenticated, login, logout, hasRole, token };
 };
