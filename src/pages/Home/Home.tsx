709655d60428d76dc992210cf9a6657de729d310
Justyna Małuszyńska <64073445+justyna-maluszynska@users.noreply.github.com>
2022-10-13 10:56:29 +0200

    Remove component from home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,47 +1,12 @@
-import dayjs from 'dayjs';
-import { useState } from 'react';
-import { Recurrence } from '~/components/Recurrence';
-import { FrequencyType } from '~/enums/FrequencyType';
-import { RecurrenceType } from '~/models/RecurrenceType';
 import { Carousel } from './components/Carousel';
+import { SearchRideForm } from './components/SearchRideForm';
+import { Wrapper } from './Home.style';
 
 export const Home = () => {
-  const today = dayjs();
-  const defaultRecurrence = {
-    startDate: today,
-    endDate: today,
-    frequencyType: FrequencyType.Weekly,
-    startTime: today,
-    frequenceOccurrences: 1,
-    weekDays: [],
-    duration: { hours: 1, minutes: 0 },
-  };
-
-  const [recurrence, setRecurrence] = useState<RecurrenceType>(defaultRecurrence);
-
-  const handleRecurrenceChange = (updatedRecurrence: RecurrenceType) => {
-    console.log(updatedRecurrence);
-    setRecurrence(updatedRecurrence);
-  };
-
   return (
-    <>
-      <Recurrence
-        recurrence={recurrence}
-        onChange={handleRecurrenceChange}
-      />
+    <Wrapper>
+      <SearchRideForm />
       <Carousel />
-    </>
+    </Wrapper>
   );
 };
-// import { SearchRideForm } from './components/SearchRideForm';
-// import { Wrapper } from './Home.style';
-
-// export const Home = () => {
-//   return (
-//     <Wrapper>
-//       <SearchRideForm />
-//       <Carousel />
-//     </Wrapper>
-//   );
-// };
