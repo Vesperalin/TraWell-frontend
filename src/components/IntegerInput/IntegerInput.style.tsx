76db6a9fc6fef7e41f5a3558969e1c040cc28ba2
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-14 23:45:20 +0200

    Add integer input

---
+++ src/components/IntegerInput/IntegerInput.style.tsx
@@ -0,0 +1,18 @@
+import { Theme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles<Theme>((theme: Theme) => ({
+  integerInput: {
+    backgroundColor: theme.palette.common.dimmedWhite,
+    color: theme.palette.common.darkNavy,
+    minWidth: '270px',
+    borderRadius: '10px',
+
+    [theme.breakpoints.down('md')]: {
+      minWidth: 'auto',
+      maxWidth: '150px',
+    },
+  },
+}));
+
+export { useStyles };
