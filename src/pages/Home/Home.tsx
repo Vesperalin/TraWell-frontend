578c73b095d7ea6338268e08029a141751ed4a08
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-15 12:44:15 +0200

    Delete leftovers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react';
-import { DoubleNumberInput } from '~/components/DoubleNumberInput';
 import { Carousel } from './components/Carousel';
 import { SearchRideForm } from './components/SearchRideForm';
 import { Wrapper } from './Home.style';
 
 export const Home = () => {
-  const [value, setValue] = useState<string | null>('');
   return (
     <Wrapper>
       <SearchRideForm />
-      <DoubleNumberInput
-        value={value}
-        setValue={setValue}
-      />
       <Carousel />
     </Wrapper>
   );
