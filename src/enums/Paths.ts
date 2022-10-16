82afdc388fe5af229bedf279623c6600bc43f9f4
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-16 17:37:17 +0200

    Update path for searching rides

--- src/enums/Paths.ts
+++ src/enums/Paths.ts
@@ -1,6 +1,7 @@
 export enum Paths {
   Home = '/',
-  SearchedRides = '/searched-rides',
+  // eslint-disable-next-line max-len
+  SearchedRides = '/searched-rides/:cityFrom/:countyFrom/:stateFrom/:countryFrom/:cityTo/:countyTo/:stateTo/:countryTo/:date/:time/:seatsAmount',
   Error = '/error',
   NotFound = '*',
 }
