fca99834308ffcfcafb9a2f0853b05cf45121464
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-18 09:22:12 +0200

    Update paths

--- src/enums/Paths.ts
+++ src/enums/Paths.ts
@@ -1,7 +1,7 @@
 export enum Paths {
   Home = '/',
   // eslint-disable-next-line max-len
-  SearchedRides = '/searched-rides/:cityFrom/:countyFrom/:stateFrom/:countryFrom/:cityTo/:countyTo/:stateTo/:countryTo/:date/:time/:seatsAmount',
+  SearchedRides = '/searched-rides/:cityFrom/:countyFrom/:stateFrom/:cityTo/:countyTo/:stateTo/:date/:time/:seatsAmount',
   Error = '/error',
   NotFound = '*',
 }
