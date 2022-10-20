28737eb4c320e5a99cd5e4c2dc3d1b8fbcb04114
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-20 19:55:02 +0200

    Add hook for getting data about ride for passenger

---
+++ src/models/Rides/RideForPassengerResponse.ts
@@ -0,0 +1,46 @@
+export interface RideForPassengerResponse {
+  ride_id: number;
+  city_from: {
+    city_id: number;
+    name: string;
+    county: string;
+    state: string;
+    lat: string;
+    lng: string;
+  };
+  city_to: {
+    city_id: number;
+    name: string;
+    county: string;
+    state: string;
+    lat: string;
+    lng: string;
+  };
+  area_from: string;
+  area_to: string;
+  start_date: string;
+  price: string;
+  seats: number;
+  recurrent: boolean;
+  automatic_confirm: boolean;
+  available_seats: number;
+  description: string;
+  driver: {
+    user_id: number;
+    first_name: string;
+    last_name: string;
+    avg_rate: string;
+    avatar: string;
+    private: boolean;
+  };
+  duration: {
+    hours: number;
+    minutes: number;
+  };
+  vehicle: {
+    vehicle_id: number;
+    make: string;
+    model: string;
+    color: string;
+  };
+}
