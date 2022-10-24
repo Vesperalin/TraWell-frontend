f01347d47a362aced3cff9b9360b97f08ce44ba4
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-24 23:32:36 +0200

    Implement taSK

--- src/pages/SearchedRideForPassenger/SearchedRideForPassenger.style.tsx
+++ src/pages/SearchedRideForPassenger/SearchedRideForPassenger.style.tsx
@@ -1,12 +1,33 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import { styled } from '@mui/system';
 
 export const Wrapper = styled(Box)({
   display: 'flex',
   flexDirection: 'column',
   width: '100%',
+  paddingBottom: '20px',
   gap: '20px',
 });
+
+export const BackButton = styled(Button)(({ theme }) => ({
+  margin: '10px 0 0 0',
+  alignSelf: 'flex-start',
+  paddingLeft: '0',
+  backgroundColor: 'transparent',
+  color: theme.palette.common.darkNavy,
+  opacity: '0.5',
+
+  '&:hover': {
+    backgroundColor: 'transparent',
+    color: '#05172C',
+    opacity: '0.7',
+  },
+
+  [theme.breakpoints.down('sm')]: {
+    margin: '5px 0 0 0',
+  },
+}));
 
 export const DataWrapper = styled(Box)(({ theme }) => ({
   display: 'flex',
