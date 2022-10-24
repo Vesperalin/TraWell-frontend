a4fa2f6f1ae80daf8023b46a76a95585d8567d26
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-24 16:11:05 +0200

    Update path

--- src/enums/Paths.ts
+++ src/enums/Paths.ts
@@ -3,7 +3,7 @@ export enum Paths {
   // eslint-disable-next-line max-len
   SearchedRides = '/searched-rides/:cityFrom/:countyFrom/:stateFrom/:latFrom/:lonFrom/:cityTo/:countyTo/:stateTo/:latTo/:lonTo/:date/:seatsAmount/:page',
   SearchedRideForPassenger = '/searched-ride/:rideId',
-  OwnRides = '/own-rides',
+  OwnRides = '/own-rides/:page',
   UserProfile = '/profile/:userId',
   Error = '/error',
   NotFound = '*',
