624270b3cf91723bb583166c50fa4e083352bce0
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-10-25 21:00:47 +0200

    add redirect to add page

--- src/pages/AddRide/pages/ChooseCreateRideType/ChooseCreateRideType.tsx
+++ src/pages/AddRide/pages/ChooseCreateRideType/ChooseCreateRideType.tsx
@@ -1,15 +1,18 @@
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 import { PrimaryButton } from '~/components/PrimaryButton';
+import { Paths } from '~/enums/Paths';
 import { Sizes } from '~/enums/StyleSettings';
 import { Wrapper } from './ChooseCreateRideType.style';
 
 export const ChooseCreateRideType = () => {
+  const navigate = useNavigate();
   return (
     <Wrapper>
       <Typography variant='h3'>Choose a ride type you want to create</Typography>
       <PrimaryButton
         label='Singular'
-        onClick={() => console.log('xd')}
+        onClick={() => navigate(Paths.AddSingularRide)}
         desktopSize={Sizes.Medium}
         mobileSize={Sizes.Medium}
       />
