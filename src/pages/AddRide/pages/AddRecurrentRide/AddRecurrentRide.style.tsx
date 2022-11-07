d59aef801ac22e9c24147936b2aa71ccfdd8d3f7
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-11-07 22:25:57 +0100

    style form

---
+++ src/pages/AddRide/pages/AddRecurrentRide/AddRecurrentRide.style.tsx
@@ -0,0 +1,59 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/system';
+
+export const Form = styled(Box)(({ theme }) => ({
+  width: '100%',
+  padding: '30px',
+  backgroundColor: theme.palette.common.darkNavy,
+  borderRadius: '10px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+}));
+
+export const Title = styled(Typography)(({ theme }) => ({
+  color: theme.palette.common.blue,
+}));
+
+export const ErrorMessage = styled(Typography)(({ theme }) => ({
+  color: theme.palette.common.lightRed,
+}));
+
+export const Label = styled(Typography)(({ theme }) => ({
+  color: theme.palette.common.dimmedWhite,
+  fontWeight: '400',
+  marginBottom: '-15px',
+}));
+
+export const FormSectionWrapper = styled(Box)({
+  padding: '10px 0',
+
+  '& > div': {
+    display: 'flex',
+    margin: '15px 0',
+  },
+});
+
+export const SectionWrapper = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  columnGap: '30px',
+});
+
+export const InnerWrapper = styled(Box)({
+  display: 'flex',
+  gap: '10px',
+  flexWrap: 'wrap',
+});
+
+export const DescriptionWrapper = styled(Box)({
+  marginBottom: '20px',
+});
+
+export const ButtonWrapper = styled(Box)({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginTop: '10px',
+});
---
+++ src/pages/AddRide/pages/AddRecurrentRide/AddRecurrentRide.tsx
@@ -0,0 +1,257 @@
+import Box from '@mui/material/Box';
+import dayjs, { Dayjs } from 'dayjs';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import RidesService from '~/api/services/RidesService';
+import { AmountOfPeopleInput } from '~/components/AmountOfPeopleInput';
+import { ChooseVehicle } from '~/components/ChooseVehicle';
+import { DatePicker } from '~/components/DatePicker';
+import { Description } from '~/components/Description';
+import { DoubleNumberInput } from '~/components/DoubleNumberInput';
+import { IntegerInput } from '~/components/IntegerInput';
+import { Modal } from '~/components/Modal';
+import { PlaceAutocompleteInput } from '~/components/PlaceAutocompleteInput';
+import { PrimaryButton } from '~/components/PrimaryButton';
+import { RadioGroup } from '~/components/RadioGroup';
+import { Recurrence } from '~/components/Recurrence';
+import { RoadMap } from '~/components/RoadMap';
+import { TextInput } from '~/components/TextInput';
+import { TimePicker } from '~/components/TimePicker';
+import { FrequencyType } from '~/enums/FrequencyType';
+import { Paths } from '~/enums/Paths';
+import { Role } from '~/enums/Role';
+import { Sizes } from '~/enums/StyleSettings';
+import { useAuth } from '~/hooks/useAuth';
+import { AutocompletePlace } from '~/models/AutocompletePlace';
+import { Duration } from '~/models/Duration';
+import { RecurrenceType } from '~/models/RecurrenceType';
+import { transformToFullDate } from '~/utils/TransformToFullDate';
+import {
+  Form,
+  Title,
+  FormSectionWrapper,
+  Label,
+  InnerWrapper,
+  SectionWrapper,
+  DescriptionWrapper,
+  ButtonWrapper,
+  ErrorMessage,
+} from './AddRecurrentRide.style';
+
+const initialState = {
+  startDate: dayjs(),
+  endDate: dayjs(),
+  startTime: dayjs(),
+  frequencyType: FrequencyType.Weekly,
+  weekDays: [],
+  frequenceOccurrences: 1,
+  duration: {
+    hours: 0,
+    minutes: 0,
+  } as Duration,
+};
+
+export const AddRecurrentRide = () => {
+  const { hasRole, token } = useAuth();
+  const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const [placeFrom, setPlaceFrom] = useState<AutocompletePlace | null>(null);
+  const [exactPlaceFrom, setExactPlaceFrom] = useState<string>('');
+  const [placeTo, setPlaceTo] = useState<AutocompletePlace | null>(null);
+  const [exactPlaceTo, setExactPlaceTo] = useState<string>('');
+  const [amountOfPeople, setAmountOfPeople] = useState<string | null>('1');
+  const [recurrence, setRecurrence] = useState<RecurrenceType>(initialState);
+  const [hours, setHours] = useState<string | null>('');
+  const [minutes, setMinutes] = useState<string | null>('');
+  const [price, setPrice] = useState<string | null>('');
+  const [vehicle, setVehicle] = useState<number | null>(null);
+  const [passengerAcceptance, setPassengerAcceptance] = useState<string>('automatic');
+  const [description, setDescription] = useState<string>('');
+  const [descriptionChecked, setDescriptionChecked] = useState<boolean>(false);
+  const [allPoints, setAllPoints] = useState<[number, number][]>([]);
+  const [mapChecked, setMapChecked] = useState<boolean>(false);
+
+  const isError = false;
+
+  // useEffect(() => {
+  //   if (placeFrom !== null && placeTo !== null) {
+  //     const tempPoints: [number, number][] = [];
+  //     tempPoints.push([Number(placeTo.lat), Number(placeTo.lon)]);
+  //     tempPoints.push([Number(placeFrom.lat), Number(placeFrom.lon)]);
+  //     setAllPoints(tempPoints);
+  //   } else {
+  //     setAllPoints([]);
+  //     setMapChecked(false);
+  //   }
+  // }, [placeFrom, placeTo]);
+
+  // const { refetch, isError } = RidesService.useAddRide(
+  //   token,
+  //   placeFrom,
+  //   placeTo,
+  //   exactPlaceFrom,
+  //   exactPlaceTo,
+  //   startDate && startTime ? transformToFullDate(startDate, startTime) : null,
+  //   price,
+  //   amountOfPeople,
+  //   vehicle,
+  //   hours,
+  //   minutes,
+  //   description,
+  //   allPoints,
+  //   passengerAcceptance,
+  //   hasRole,
+  // );
+
+  const submitHandler = () => {
+    // if (placeFrom === null) {
+    //   setError('You have to choose starting place - "Place from"');
+    // } else if (placeTo === null) {
+    //   setError('You have to choose destination place - "Place to"');
+    // } else if (startDate === null) {
+    //   setError('You have to choose start date');
+    // } else if (startTime === null) {
+    //   setError('You have to choose start time');
+    // } else if (!startDate.isValid()) {
+    //   setError('You have to choose valid date');
+    // } else if (!startTime.isValid()) {
+    //   setError('You have to choose valid time');
+    // } else if (amountOfPeople === null || amountOfPeople.trim() === '') {
+    //   setError('You have to choose amount of people');
+    // } else if (hours === null || hours.trim() === '') {
+    //   setError('You have to choose hours. If none, give: 0');
+    // } else if (minutes === null || minutes.trim() === '') {
+    //   setError('You have to choose minutes. If none, give: 0');
+    // } else if (price === null || price.trim() === '') {
+    //   setError('You have to choose price per passenger');
+    // } else if (hasRole(Role.Private) && vehicle === null) {
+    //   setError('You have to choose vehicle');
+    // } else if (exactPlaceFrom.length > 100) {
+    //   setError('"Exact place from" is too long. Maximum is 100 signs.');
+    // } else if (exactPlaceTo.length > 100) {
+    //   setError('"Exact place to" is too long. Maximum is 100 signs.');
+    // } else {
+    //   refetch().then(() => {
+    //     setOpenModal(true);
+    //   });
+    // }
+  };
+
+  return (
+    <Form>
+      <Title variant='h3'>Add a ride</Title>
+      {error !== '' && <ErrorMessage variant='h4'>{error}</ErrorMessage>}
+      <FormSectionWrapper>
+        <Label variant='body2'>From</Label>
+        <PlaceAutocompleteInput
+          label='Place from'
+          value={placeFrom}
+          setValue={setPlaceFrom}
+        />
+        <TextInput
+          label='Exact place from (optional)'
+          value={exactPlaceFrom}
+          setValue={setExactPlaceFrom}
+        />
+      </FormSectionWrapper>
+      <FormSectionWrapper>
+        <Label variant='body2'>To</Label>
+        <PlaceAutocompleteInput
+          label='Place to'
+          value={placeTo}
+          setValue={setPlaceTo}
+        />
+        <TextInput
+          label='Exact place to (optional)'
+          value={exactPlaceTo}
+          setValue={setExactPlaceTo}
+        />
+      </FormSectionWrapper>
+      <SectionWrapper>
+        <FormSectionWrapper>
+          <Label variant='body2'>Amount of people</Label>
+          <AmountOfPeopleInput
+            amountOfPeople={amountOfPeople}
+            setAmountOfPeople={setAmountOfPeople}
+          />
+        </FormSectionWrapper>
+        <FormSectionWrapper>
+          <Label variant='body2'>Price</Label>
+          <DoubleNumberInput
+            value={price}
+            setValue={setPrice}
+          />
+        </FormSectionWrapper>
+        {hasRole(Role.Private) && (
+          <FormSectionWrapper>
+            <Label variant='body2'>Vehicle</Label>
+            <ChooseVehicle
+              value={vehicle}
+              setValue={setVehicle}
+            />
+          </FormSectionWrapper>
+        )}
+        {hasRole(Role.Company) && (
+          <FormSectionWrapper>
+            <Label variant='body2'>Passenger acceptance</Label>
+            <RadioGroup
+              options={[
+                { value: 'automatic', label: 'Automatic' },
+                { value: 'manual', label: 'Manual' },
+              ]}
+              defaultValue='automatic'
+              setValue={setPassengerAcceptance}
+            />
+          </FormSectionWrapper>
+        )}
+      </SectionWrapper>
+      <FormSectionWrapper>
+        <Recurrence
+          recurrence={recurrence}
+          onChange={setRecurrence}
+        />
+      </FormSectionWrapper>
+      <DescriptionWrapper>
+        <Description
+          label='Description (optional)'
+          value={description}
+          setValue={setDescription}
+          checked={descriptionChecked}
+          setChecked={setDescriptionChecked}
+        />
+      </DescriptionWrapper>
+      <RoadMap
+        coordinates={allPoints}
+        heightOfMap={500}
+        isEditable={true}
+        setPoints={setAllPoints}
+        label='Road map (optional)'
+        checked={mapChecked}
+        setChecked={setMapChecked}
+      />
+      <ButtonWrapper>
+        <PrimaryButton
+          label='Add ride'
+          onClick={submitHandler}
+          desktopSize={Sizes.Medium}
+          mobileSize={Sizes.Small}
+        />
+      </ButtonWrapper>
+      <Modal
+        open={openModal}
+        title={isError ? 'Error occurred' : 'Ride added successfully'}
+        text={
+          isError
+            ? 'Unexpected error occurred while adding the ride. Ride was not created!'
+            : 'The ride was added successfully. Wait for first passengers to book a place!'
+        }
+        showButtonForOpeningModal={false}
+        handleOpen={() => setOpenModal(true)}
+        handleClose={() => navigate(Paths.Home)}
+        primaryButtonText='Okay'
+        primaryButtonAction={() => navigate(Paths.Home)}
+      />
+    </Form>
+  );
+};
