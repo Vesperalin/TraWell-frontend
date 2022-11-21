f7c7d00a25e3ea8ebb50f94fd6084d4acb6b441e
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-11-21 15:13:04 +0100

    ZT-209-Add users vehicles in forms (#55)

--- src/components/ChooseVehicle/ChooseVehicle.tsx
+++ src/components/ChooseVehicle/ChooseVehicle.tsx
@@ -1,9 +1,15 @@
+import { Typography } from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { Theme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { StyledFormControl } from './ChooseVehicle.style';
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import UsersService from '~/api/services/UsersService';
+import { Paths } from '~/enums/Paths';
+import { useAuth } from '~/hooks/useAuth';
+import { StyledFormControl, StyledSkeleton } from './ChooseVehicle.style';
 
 interface Props {
   value: number | null;
@@ -12,9 +18,31 @@ interface Props {
 
 export const ChooseVehicle = ({ value, setValue }: Props) => {
   const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
-  // TODO: add request to DB and add async handling
-  // ------ (display textfield instead of MenuItems when loading or sth)
-  // TODO set first vehicle as default if exist
+  const { token } = useAuth();
+  const {
+    data: idUserData,
+    refetch: refetchIdUserData,
+    isError: isErrorUserData,
+    isLoading: isLoadingUserData,
+  } = UsersService.useGetMyId(token ? token : '');
+  const {
+    data: vehicles,
+    refetch: refetchVehicles,
+    isError: isErrorVehicles,
+    isLoading: isLoadingVehicles,
+  } = UsersService.useGetUserVehicles(token ? token : '', idUserData ? idUserData.user_id : -1);
+
+  useEffect(() => {
+    if (token) {
+      refetchIdUserData();
+    }
+  }, [refetchIdUserData, token]);
+
+  useEffect(() => {
+    if (token && idUserData) {
+      refetchVehicles();
+    }
+  }, [idUserData, refetchVehicles, token]);
 
   const handleChange = (event: SelectChangeEvent) => {
     if (event.target.value != '-1') {
@@ -23,6 +51,27 @@ export const ChooseVehicle = ({ value, setValue }: Props) => {
       setValue(null);
     }
   };
+
+  if (isErrorUserData || isErrorVehicles) {
+    return (
+      <Navigate
+        to={Paths.Error}
+        replace={true}
+        state={{
+          text: 'Unexpected error appeared during retrieving data. Try again',
+        }}
+      />
+    );
+  }
+
+  if (isLoadingUserData || isLoadingVehicles) {
+    return (
+      <StyledSkeleton
+        variant='rectangular'
+        height={40}
+      />
+    );
+  }
 
   return (
     <StyledFormControl size={isSmallScreen ? 'small' : 'medium'}>
@@ -34,26 +83,20 @@ export const ChooseVehicle = ({ value, setValue }: Props) => {
         onChange={handleChange}
         size={isSmallScreen ? 'small' : 'medium'}
       >
-        <MenuItem value={1}>Opel, Astra, Czarny 1</MenuItem>
-        <MenuItem value={2}>Opel, Astra, Czarny 2</MenuItem>
-        <MenuItem value={3}>Opel, Astra, Czarny 3</MenuItem>
-        <MenuItem value={4}>Opel, Astra, Czarny 4</MenuItem>
-        <MenuItem value={5}>Opel, Astra, Czarny 5</MenuItem>
-        <MenuItem value={6}>Opel, Astra, Czarny 6</MenuItem>
-        <MenuItem value={7}>Opel, Astra, Czarny 7</MenuItem>
-        <MenuItem value={8}>Opel, Astra, Czarny 8</MenuItem>
-        <MenuItem value={9}>Opel, Astra, Czarny 9</MenuItem>
-        <MenuItem value={10}>Opel, Astra, Czarny 10</MenuItem>
-        <MenuItem value={11}>Opel, Astra, Czarny 11</MenuItem>
-        <MenuItem value={12}>Opel, Astra, Czarny 12</MenuItem>
-        <MenuItem value={13}>Opel, Astra, Czarny 13</MenuItem>
-        <MenuItem value={14}>Opel, Astra, Czarny 14</MenuItem>
-        <MenuItem value={15}>Opel, Astra, Czarny 15</MenuItem>
-        <MenuItem value={16}>Opel, Astra, Czarny 16</MenuItem>
-        <MenuItem value={17}>Opel, Astra, Czarny 17</MenuItem>
-        <MenuItem value={18}>Opel, Astra, Czarny 18</MenuItem>
-        <MenuItem value={19}>Opel, Astra, Czarny 19</MenuItem>
-        <MenuItem value={20}>Opel, Astra, Czarny 20</MenuItem>
+        {vehicles && Array.isArray(vehicles) ? (
+          vehicles.map((vehicle) => {
+            return (
+              <MenuItem
+                key={vehicle.vehicle_id}
+                value={vehicle.vehicle_id}
+              >
+                {vehicle.make}, {vehicle.model}, {vehicle.color}
+              </MenuItem>
+            );
+          })
+        ) : (
+          <Typography>No vehicles</Typography>
+        )}
       </Select>
     </StyledFormControl>
   );
