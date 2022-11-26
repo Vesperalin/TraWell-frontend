ab8239aa169e6077b1bee83ba3010fad6ed2c490
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-11-26 15:23:36 +0100

    change console.log

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -23,7 +23,7 @@ export const PrivateRoute = ({ element, role }: Props) => {
       }
     };
     check();
-    console.log('');
+    console.log();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
